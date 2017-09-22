9ad2ff0f59188ccdb308ee2cc20499bcc2493368
Ross Ragsdale <ross.ragsdale@gmail.com>
2017-09-22 00:32:26 -0500

    Updates.

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-var app = angular.module('plunker', []);
-
-app
+angular
+.module('plunker', [])
 .controller('MainCtrl', function($scope, $document) {
   $scope.lines = []
   $scope.enter = function(){
     $scope.lines.push(`guest@rosspc:~/$ ${$scope.gutter}`)
-    $scope.lines.push(`Command ${$scope.gutter} not recognized. Please try again.`)
+    switch($scope.gutter) {
+      case 'ls':
+        $scope.lines.push(`README.txt`)
+        break;
+      case 'pwd':
+        $scope.lines.push(`/Users/guest`)
+        break;
+      default:
+        $scope.lines.push(`command not found: ${$scope.gutter}`)
+    }
     delete $scope.gutter
   }
 })
